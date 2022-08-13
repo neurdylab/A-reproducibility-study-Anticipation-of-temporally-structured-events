9e38beaa012d3ba7966bdc3e65fc08187859607a
rithwikrg <111189196+rithwikrg@users.noreply.github.com>
2022-08-13 11:02:40 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -2,9 +2,10 @@ import glob
 import pickle
 import tables
 import numpy as np
+import pandas as pd
 import nibabel as nib
 from s_light import get_s_lights
-from utils import save_nii
+from utils import save_nii, load_sliced_nii
 
 def find_valid_vox(fpath, subjects, min_subjs=15):
     """Loads data files to define valid_vox.nii
@@ -23,37 +24,35 @@ def find_valid_vox(fpath, subjects, min_subjs=15):
     """
 
     D_num_not_nan = []
-    MNI_mask_path = 'MNI152_T1_brain_resample.nii'
-    print("Loading data", end='', flush=True)
-    for rep in range(6):
-        D_rep = np.zeros((121, 145, 121, 60))
-        D_not_nan = np.zeros((121, 145, 121))
+    MNI_mask_path = fpath + 'scripts/preproc/MNI152_T1_brain_resample.nii' # used in both mainproc and preproc, should have in both?
 
-        for subj in subjects:
-            print('.', end='', flush=True)
-            fname = glob.glob(fpath + subj + '/*Intact*' +
-                              str(rep + 1) + '.nii')
+    for subj in subjects:
+        for run in range(3):
+            sub_value = subj[subj.find('sub'):subj.find('sub')+6] # this takes subj and returns sub-01, etc
+            run_value = 'run-0{}'.format(run+1) # this takes run # and returns run-01, etc
+            fname = fpath + 'pre_outputs/{}/{}_{}_tf_func.nii.gz'.format(sub_value, sub_value, run_value)
+            tsv_fpath = fpath + 'raw_data/{}/func/{}_task-movie_{}_events.tsv'.format(sub_value, sub_value, run_value)
+            intact_data = load_sliced_nii(fname, tsv_fpath) # this is added to divide clips on the fly
+            D_rep = np.zeros((121, 145, 121, 60))
+            D_not_nan = np.zeros((121, 145, 121))
 
-            assert len(fname) == 1, \
-                   "More than one file found for subject " + subj
+            for i in range(len(intact_data)):
+                rep_z = intact_data[i].T
+                nnan = ~np.all(rep_z == 0, axis=3) # nnan stands for not NaN
+                rep_mean = np.mean(rep_z[nnan], axis=1, keepdims=True)
+                rep_std = np.std(rep_z[nnan], axis=1, keepdims=True)
+                rep_z[nnan] = (rep_z[nnan] - rep_mean)/rep_std # z scoring
 
-            rep_z = nib.load(fname[0]).get_fdata()
-            nnan = ~np.all(rep_z == 0, axis=3)
-            rep_mean = np.mean(rep_z[nnan], axis=1, keepdims=True)
-            rep_std = np.std(rep_z[nnan], axis=1, keepdims=True)
-            rep_z[nnan] = (rep_z[nnan] - rep_mean)/rep_std
+                D_rep[nnan] += rep_z[nnan] # this is not used in this function, no need to calc
+                D_not_nan[nnan] += 1 # adding 1 at every voxel that is 'valid' (not nan) from array of zeros
 
-            D_rep[nnan] += rep_z[nnan]
-            D_not_nan[nnan] += 1
+            D_num_not_nan.append(D_not_nan.T) # putting together all 3 runs for one subj
 
-        D_num_not_nan.append(D_not_nan.T)
-    print(' ')
-
-    non_nan_mask = np.min(D_num_not_nan, axis=0) # Min across reps
+    non_nan_mask = np.min(D_num_not_nan, axis=0) # Min across reps (if value is 0 for any one of three reps, that is chosen)
     MNI_mask = nib.load(MNI_mask_path).get_fdata().astype(bool).T
-    non_nan_mask = (non_nan_mask > (min_subjs - 1)) * MNI_mask
+    non_nan_mask = (non_nan_mask > (min_subjs - 1)) * MNI_mask # combined with MNI mask
 
-    save_nii(fpath + 'valid_vox.nii', fname, non_nan_mask)
+    save_nii(fpath + 'pre_outputs/valid_vox.nii', fname, non_nan_mask)
 
 def save_s_lights(fpath, non_nan_mask, savepath):
     """Save a separate data file for each searchlight
@@ -71,37 +70,41 @@ def save_s_lights(fpath, non_nan_mask, savepath):
         Path to directory to save data files
     """
 
-    subjects = glob.glob(fpath + '*pred*')
-    coords = np.transpose(np.where(non_nan_mask))
-    SL_allvox = get_s_lights(coords)
+    subjects = glob.glob(fpath + '*sub*')
+    coords = np.transpose(np.where(non_nan_mask)) # gets coords based on valid voxels from non_nan_mask
+    SL_allvox = get_s_lights(coords) # every voxel that meets certain conditions gets turned into search light
     pickle.dump(SL_allvox, open(savepath + 'SL_allvox.p', 'wb'))
-    nSL = len(SL_allvox)
+    nSL = 1 #len(SL_allvox) hardcoded for testing purposes
 
-    for subj in subjects:
-        subjname = 'subj_' + subj.split('/')[-1]
-        print(subjname)
-        for cond in ['Intact', 'SFix', 'SRnd']:
-            print("   " + cond)
+    for subj in subjects: # iterates over each subj (folder w/ 3 runs) and then over conditions (6 clips of each cond per subj)
+        for cond in ['Intact', 'Fix', 'Rnd']:
+            subjname = 'subj_' + subj.split('/')[-1]
+
+            # this is added to divide clips on the fly
+            sub_value = subj[subj.find('sub'):subj.find('sub')+6]
+            all_intact_data = []
+            for run in range(3):
+                run_value = 'run-0{}'.format(run+1)
+                intact_data = load_sliced_nii(sub_value, run_value, cond)
+                all_intact_data.extend(intact_data)
+        
             all_rep = []
-            for rep in range(6):
+            for i in range(6):
                 # Load and z-score data
-                fname = glob.glob(fpath + subj + '/filt*' +
-                                  cond + '*' + str(rep + 1) + '.nii')
-                rep_z = nib.load(fname[0]).get_fdata().T
+                rep_z = all_intact_data[i]
                 rep_z = rep_z[:, non_nan_mask]
                 nnan = ~np.all(rep_z == 0, axis=0)
                 rep_mean = np.mean(rep_z[:,nnan], axis=0, keepdims=True)
                 rep_std = np.std(rep_z[:,nnan], axis=0, keepdims=True)
                 rep_z[:,nnan] = (rep_z[:,nnan] - rep_mean)/rep_std
-                all_rep.append(rep_z)
+                all_rep.append(rep_z) # z scored clip of len 60 being attached to all_rep
 
             # Append to SL hdf5 files
             for sl_i in range(nSL):
-                sl_data = np.zeros((6, 60, len(SL_allvox[sl_i])))
-                for rep in range(6):
-                    sl_data[rep,:,:] = all_rep[rep][:,SL_allvox[sl_i]]
-                h5file = tables.open_file(savepath + str(sl_i) + '.h5',
-                                          mode='a')
+                sl_data = np.zeros((6, 60, len(SL_allvox[len(SL_allvox) - sl_i - 1]))) # iterating from the end because the first few are nans
+                for i in range(6):
+                    sl_data[i,:,:] = all_rep[i][:,SL_allvox[len(SL_allvox) - sl_i - 1]] # using coords of SL_allvox to find matching values in all_rep for each sl
+                h5file = tables.open_file(savepath + str(sl_i) + '.h5', mode='a')
 
                 if '/' + subjname not in h5file:
                     h5file.create_group('/', subjname)
--- main.py
+++ main.py
@@ -10,25 +10,28 @@ from s_light import optimal_events, compile_optimal_events, \
                     shift_corr, compile_shift_corr
 
 nSL = 5354
-nPerm = 100
+nPerm = 3 #100
 max_lag = 10
-subjects = glob.glob('../data/*predtrw*')
-header_fpath = '../data/0407161_predtrw02/filtFuncMNI_Intact_Rep1.nii'
+
+fpath = '/data1/neurdylab/bayrakrg/anticipation/'
+header_fpath = fpath + 'pre_outputs/sub-01/sub-01_run-01_tf_func.nii.gz'
+subjects = glob.glob(fpath + 'pre_outputs/*sub*')
 
 # Create valid_vox.nii mask
-find_valid_vox('../data/', subjects)
+find_valid_vox(fpath, subjects)
 
 # Create a separate data file for each searchlight
-non_nan = nib.load('../data/valid_vox.nii').get_fdata().T > 0
-save_s_lights('../data/', non_nan, '../data/SL/')
+non_nan = nib.load(fpath + 'pre_outputs/valid_vox.nii').get_fdata().T > 0
+save_s_lights(fpath + 'pre_outputs/', non_nan, fpath + 'pre_outputs/SL/')
 
 # Run all analyses in each searchlight
 # This will take ~1000 CPU hours, and so should be run
 # in parallel on a cluster if possible
+
 for sl_i in range(nSL):
 
     # Load data for this searchlight
-    sl_h5 = tables.open_file('../data/SL/%d.h5' % sl_i, mode='r')
+    sl_h5 = tables.open_file(fpath + 'pre_outputs/SL/%d.h5' % sl_i, mode='r')
     data_list_orig = []
     for subj in subjects:
         subjname = '/subj_' + subj.split('/')[-1]
@@ -36,7 +39,6 @@ for sl_i in range(nSL):
         data_list_orig.append(d)
     sl_h5.close()
     nSubj = len(data_list_orig)
-
 
     sl_K = []
     sl_seg = []
@@ -60,18 +62,22 @@ for sl_i in range(nSL):
 
     # Save results for this searchlight
     pickle.dump(sl_K,
-                open('../outputs/perm/optimal_events_%d.p' % sl_i, 'wb'))
+                open(fpath + 'processed_data/perm/optimal_events_%d.p' % sl_i, 'wb'))
     pickle.dump(sl_seg,
-                open('../outputs/perm/fit_HMM_%d.p' % sl_i, 'wb'))
+                open(fpath + 'processed_data/perm/fit_HMM_%d.p' % sl_i, 'wb'))
     pickle.dump(sl_shift_corr,
-                open('../outputs/perm/shift_corr_%d.p' % sl_i, 'wb'))
+                open(fpath + 'processed_data/perm/shift_corr_%d.p' % sl_i, 'wb'))
 
 # Compile results into final maps
-SL_allvox = pickle.load(open('../data/SL/SL_allvox.p', 'rb'))
-compile_optimal_events('../outputs/perm/', non_nan, SL_allvox,
-                        header_fpath, '../outputs/')
-opt_event = nib.load('optimal_events.nii').get_fdata().T
-compile_fit_HMM('../outputs/perm/', non_nan, SL_allvox,
-                header_fpath, '../outputs/', opt_event)
-compile_shift_corr('../outputs/perm/', non_nan, SL_allvox,
-                   header_fpath, '../outputs')
+SL_allvox = pickle.load(open(fpath + 'pre_outputs/SL/SL_allvox.p', 'rb'))
+#SL_allvox = list(reversed(SL_allvox[5791:5792]))
+
+compile_optimal_events(fpath + 'processed_data/perm/', non_nan, SL_allvox,
+                        header_fpath, fpath + 'processed_data/')
+
+opt_event = nib.load(fpath + 'processed_data/optimal_events.nii').get_fdata().T
+compile_fit_HMM(fpath + 'processed_data/perm/', non_nan, SL_allvox,
+                header_fpath, fpath + 'processed_data/', opt_event)
+
+compile_shift_corr(fpath + 'processed_data/perm/', non_nan, SL_allvox,
+                   header_fpath, fpath + 'processed_data/')
